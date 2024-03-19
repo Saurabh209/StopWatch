4863da6facd55044c9a08a484865e41e7956aa1e
Saurabh209 <saurabhhh2613@gmail.com>
2024-03-19 18:17:11 +0530

    User interface complete

---
+++ script.js
@@ -0,0 +1,14 @@
+const startButton = document.getElementById("start");
+const stopButton = document.getElementById("stop");
+const resetButton = document.getElementById("reset");
+
+const minutes = document.getElementById("minutes");
+const seconds = document
+
+
+
+function onStart(){
+
+}
+
+startButton.addEventListener('click',onStart);
